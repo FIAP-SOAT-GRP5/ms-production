515e171dde9e69aa8646ff3fa19e934c5deb6504
Rodrigo Pillar Vianna <rpvianna2009@gmail.com>
2024-03-09 17:53:52 -0300

    Criadas filas SAGA para criação e cancelamento de pedido

--- src/domain/application/interfaces/order/update-order-status.use-case.interface.ts
+++ src/domain/application/interfaces/order/update-order-status.use-case.interface.ts
@@ -2,9 +2,10 @@
 import { Order } from '../../../../domain/enterprise/entities/order.entity';
 
 export interface IUpdateOrderStatusUseCase {
-  updateStatusReceived(id: number): Promise<Order>;
-  updateStatusProcessing(id: number): Promise<Order>;
-  updateStatusReady(id: number): Promise<Order>;
-  updateStatusFinished(id: number): Promise<Order>;
+	updateStatusReceived(id: number): Promise<Order>;
+	updateStatusProcessing(id: number): Promise<Order>;
+	updateStatusReady(id: number): Promise<Order>;
+	updateStatusFinished(id: number): Promise<Order>;
+	updateStatusCanceled(id: number): Promise<Order>;
 }
 /* v8 ignore start */
